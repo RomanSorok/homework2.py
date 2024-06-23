0458dbbde5762618680b26c0905c24c742d28898
RomanSorok <romka.22.12.kalach@gmail.com>
2024-06-23 17:49:02 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,8 +1,5 @@
-name = "Роман"
-print(name)
-age = 26
-print(age)
-age = age + 4
-print(age)
-is_student = True
-print(is_student)
+Number_of_completed_remote_sensing = 12
+NumberOfHoursSpent = 1.5
+Course_name = "Python"
+TimePerTask = NumberOfHoursSpent / Number_of_completed_remote_sensing
+print("Количество выполненных ДЗ: ", NumberOfHoursSpent, "Количество затраченных часов:", NumberOfHoursSpent,"Название курса:", Course_name, "Время на одно задание:",TimePerTask )
